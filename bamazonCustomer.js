c83b582feba3cb2bbe1ef6490b7524e24309d0b5
Meredith Erb <mereditherb@guest-2-00478.dhcp.unc.edu>
2017-02-28 20:26:21 -0500

    added prompt for user to continue shopping and redo shop function if wants to continue or leaves app if done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -47,33 +47,45 @@ var shop = function () {
 				var stock = productItem.stock_quantity;
 				var itemID = productItem.item_id;
 				var itemCost = parseFloat(productItem.price);
-				console.log("Stock " + stock);
-				console.log("Cost " + itemCost);
+				// console.log("Stock " + stock);
+				// console.log("Cost " + itemCost);
 				inquirer.prompt({
 					type: "input",
 					name: "units",
 					message: `How many ${productItem.product_name}'s would you like to purchase?`
 				}).then(function(inventory) {
 					// console.log(stock);
-					console.log("Number wanted: " + inventory.units);
+					// console.log("Number wanted: " + inventory.units);
 					if (parseInt(stock) >= parseInt(inventory.units)) {
 						var newQuantity = (stock - parseInt(inventory.units));
 						var totalCost = (parseFloat(inventory.units) * parseFloat(itemCost)).toFixed(2);
-						console.log("New Quantity: " + newQuantity);
-						console.log("ID of product wanted: " + itemID);
+						// console.log("New Quantity: " + newQuantity);
+						// console.log("ID of product wanted: " + itemID);
 						console.log("Total Cost: $" + totalCost);
 						connection.query("UPDATE products SET ? WHERE ?", [{
 							//need to subtract
 							stock_quantity: newQuantity
 						}, {
 							item_id: itemID
-						}], function (err, res){});//may need to add back in table creation
+						}], function (err, res){});
 						//end of updating db
-						displayTable();
+						inquirer.prompt([{
+						type: "confirm",
+						name: "id",
+						message: "Would you like to order more?"
+						}]).then(function(continueShop) {
+							if (continueShop.id === true ) {
+								saleItems = [];
+								shop();
+							}
+							else {
+								console.log("Have a Nice Day!!");
+								connection.end();
+							}
+						});
 					}//end of if
 					else {
-						console.log("Insufficient Quantity");
-						// shop();
+						console.log("Insufficient Quantity!!!");
 					}
 				});//end of inventory fn
 			} //end of if
@@ -85,20 +97,21 @@ var shop = function () {
 
 shop();
 
-var displayTable = function () {
-		connection.query("SELECT * FROM products", function(err, res) {
-			// console.log("test: " + res);
-				saleItems=[];
-				// if (err) throw err;
-				for (var i = 0; i < res.length; i++){
-					saleItems.push({
-						ID: res[i].item_id, 
-						Product: res[i].product_name, 
-						Department: res[i].department_name, 
-						Price: res[i].price, 
-						Quantity: res[i].stock_quantity
-					});
-				}//end of for loop
-				console.table(saleItems);
-		});
-	};
+// var displayTable = function () {
+// 		connection.query("SELECT * FROM products", function(err, res) {
+// 			// console.log("test: " + res);
+// 				saleItems=[];
+// 				// if (err) throw err;
+// 				for (var i = 0; i < res.length; i++){
+// 					saleItems.push({
+// 						ID: res[i].item_id, 
+// 						Product: res[i].product_name, 
+// 						Department: res[i].department_name, 
+// 						Price: res[i].price, 
+// 						Quantity: res[i].stock_quantity
+// 					});
+// 				}//end of for loop
+// 				console.table(saleItems);
+// 				shop();
+// 		});
+// 	};
